d6a00bcd87c052441e340b29bcb3c6fb51dee444
Ayatooo <l.reynard@isi-dsi.fr>
2023-11-02 16:44:22 +0100

    :zap: in progress

--- src/components/atoms/ProductList.jsx
+++ src/components/atoms/ProductList.jsx
@@ -8,7 +8,6 @@ import SearchBar from "../atoms/SearchBar";
 
 const ProductList = () => {
     const [filteredProducts, setFilteredProducts] = useState([]);
-    const [oldProducts, setOldProducts] = useState([]);
     const [loading, setLoading] = useState(true);
     const [apiPage, setApiPage] = useState(1);
     const [searchTerm, setSearchTerm] = useState("");
@@ -17,27 +16,34 @@ const ProductList = () => {
 
     const fetchCategories = async () => {
         axios(api("get", `categories`))
-        .then((response) => {
-            setCategories(response.data.data);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+            .then((response) => {
+                setCategories(response.data.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     };
 
     useEffect(() => {
         fetchCategories();
     }, []);
 
-    const fetchProducts = async (page) => {
-        axios(api("get", `products?page=${page}&search=${searchTerm}`))
-        .then((response) => {
-            setFilteredProducts([...filteredProducts, ...response.data.data]);
-            setLoading(false);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+    const fetchProducts = async () => {
+        let data = [
+            {
+                page: apiPage,
+                search: searchTerm,
+                categories: activeCategories
+            },
+        ];
+        axios(api("post", "products", data))
+            .then((response) => {
+                setFilteredProducts([...filteredProducts, ...response.data.data]);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     };
 
     useEffect(() => {
@@ -46,13 +52,20 @@ const ProductList = () => {
 
     const handleSearch = (query) => {
         setSearchTerm(query);
-        axios(api("get", `products?page=${apiPage}&search=${searchTerm}`))
-        .then((response) => {
-            setFilteredProducts(response.data.data);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        let data = [
+            {
+                page: 1,
+                search: searchTerm,
+                categories: activeCategories
+            },
+        ];
+        axios(api("post", "products", data))
+            .then((response) => {
+                setFilteredProducts(response.data.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     };
 
     const handleFilter = (categoryId) => {
@@ -64,28 +77,22 @@ const ProductList = () => {
     };
 
     useEffect(() => {
-        axios(api("get", `products?category=${activeCategories.join(",")}`))
-        .then((response) => {
-            setFilteredProducts(response.data.data);
-        })
-        .catch((err) => {
-            console.error(err);
-        });
+        let data = [
+            {
+                page: 1,
+                search: searchTerm,
+                categories: activeCategories
+            },
+        ];
+        axios(api("post", "products", data))
+            .then((response) => {
+                console.log(response.data.data);
+                setFilteredProducts(response.data.data);
+            })
+            .catch((err) => {
+                console.error(err);
+            });
     }, [activeCategories]);
-
-    const skeletonCards = Array.from({ length: 10 }, (_, index) => (
-        <div key={index} className="w-full skeleton-loader">
-            <div className="bg-white rounded-md shadow-lg overflow-hidden animate-pulse">
-                <div className="h-64 bg-gray-300"></div>
-                <div className="p-6">
-                    <div className="h-4 bg-gray-300 mb-4"></div>
-                    <div className="h-4 bg-gray-300 mb-4"></div>
-                    <div className="h-4 bg-gray-300 mb-4"></div>
-                    <div className="h-4 bg-gray-300"></div>
-                </div>
-            </div>
-        </div>
-    ));
 
     return (
         <InfiniteScroll
@@ -93,39 +100,35 @@ const ProductList = () => {
             next={() => setApiPage(apiPage + 1)}
             hasMore={true}
         >
-        <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-20">
-            <div className="mx-auto max-w-2xl lg:max-w-none">
-                <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mb-4">Produits 📪</h2>
-                <p className="text-lg leading-8 mb-8">Découvrez toutes les offres de nos marchands.</p>
-                <SearchBar onSearch={handleSearch} />
-                <div className="flex flex-wrap gap-4">
-                    {categories.map((category) => (
-                        <button
-                            key={category.id}
-                            className={`${
-                                activeCategories.includes(category.id)
+            <div className="mx-auto max-w-7xl px-6 lg:px-8 mt-20">
+                <div className="mx-auto max-w-2xl lg:max-w-none">
+                    <h2 className="text-3xl font-bold tracking-tight sm:text-4xl mb-4">Produits 📪</h2>
+                    <p className="text-lg leading-8 mb-8">Découvrez toutes les offres de nos marchands.</p>
+                    <SearchBar onSearch={handleSearch} />
+                    <div className="flex flex-wrap gap-4">
+                        {categories.map((category) => (
+                            <button
+                                key={category.id}
+                                className={`${activeCategories.includes(category.id)
                                     ? "bg-blue-500 text-white"
                                     : "bg-blue-100 text-blue-500"
-                            } px-4 py-2 rounded-md font-medium text-sm`}
-                            onClick={() => handleFilter(category.id)}
-                        >
-                            {category.name}
-                        </button>
-                    ))}
+                                    } px-4 py-2 rounded-md font-medium text-sm`}
+                                onClick={() => handleFilter(category.id)}
+                            >
+                                {category.name}
+                            </button>
+                        ))}
+                    </div>
                 </div>
-            </div>
-            <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 sm:gap-y-20 sm:gap-x-5">
-                {loading ? (
-                    skeletonCards
-                ) : (
-                    filteredProducts.map((product, index) => (
+                <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3 sm:gap-y-20 sm:gap-x-5">
+                    {filteredProducts.map((product, index) => (
                         <div key={index} className="w-full">
                             <ProductCard product={product} />
                         </div>
                     ))
-                )}
+                    }
+                </div>
             </div>
-        </div>
         </InfiniteScroll>
     );
 };
--- src/index.js
+++ src/index.js
@@ -3,19 +3,10 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { ThemeProvider } from 'styled-components';
-
-const providedTheme = {
-  color: {
-    primary: 'red',
-  }
-};
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider theme={providedTheme}>
       <App />
-    </ThemeProvider>
   </React.StrictMode>
 );
 
